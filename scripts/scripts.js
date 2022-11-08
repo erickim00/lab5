f65b383e48ca1d8e2d777872ad5ad5e53f42b667
Eric <skim588@my.bcit.ca>
2022-11-08 13:04:41 -0800

    Created a for loop to iterate over the courselist to check if input is included in any of the course code

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,31 +1,28 @@
+let acit1620 = {
+    code: "ACIT 1620", 
+    name: "Fundamental Web Technologies",
+}
 
+let acit1515 = {
+    code: "ACIT 1515", 
+    name: "Scripting for IT",
+}
 
+let comm1116 = {
+    code: "COMM 1116", 
+    name: "Business Communications",
+}
 
-// let acit1620 = {
-//     code: "ACIT 1620", 
-//     name: "Fundamental Web Technologies",
-// }
-
-// let acit1515 = {
-//     code: "ACIT 1515", 
-//     name: "Scripting for IT",
-// }
-
-// let comm1116 = {
-//     code: "COMM 1116", 
-//     name: "Business Communications",
-// }
-
-// const courseList = [acit1620, acit1515, comm1116]
+const courseList = [acit1620, acit1515, comm1116]
 
 
 let input = "";
 do{
     input = prompt("Enter a four digit number: ");
-} while ((isNaN(input)) || input.length != 4);
-
-
-//     for(let course of courseList)
-//     {
-//         if (input in course)
-//         console.log(`Yes I am taking the course: ${course.code} - ${course.name}`)
+  
+} while ((isNaN(input)) || input.length != 4)
+for(let course of courseList)
+{
+    if (course.code.includes(input))
+    console.log(`Yes I am taking the course: ${course.code} - ${course.name}`)
+}
