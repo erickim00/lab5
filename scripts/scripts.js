dad76d76e6b9db4a6977a22861323a7e7b8d4f74
Eric <skim588@my.bcit.ca>
2022-11-08 11:38:22 -0800

    Edited the courseList, made each course into an object, added each course to the courseList

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
-const courseList = {
-    "ACIT 1620": "Web Fundamental Technologies",
-    "ACIT 1515": "Scripting for IT",
-    "COMM 1116": "Business Communications",
-}
+let acit1620 = {code: "ACIT 1620", name: "Fundamental Web Technologies"}
+let acit1515 = {code: "ACIT 1515", name: "Scripting for IT"}
+let comm1116 = {code: "COMM 1116", name: "Business Communications"}
+const courseList = [acit1620, acit1515, comm1116 ]
+
+
